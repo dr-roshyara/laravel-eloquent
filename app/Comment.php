fa02479d13a2dc825b93bae813e88688c7c17329
Roshyara <Roshyara@algi-hydraulik.local>
2020-09-02 14:38:05 +0200

    Today I learned elequent relationship  find and where method  used in  a Model

---
+++ app/Comment.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+use App\Post;
+use Illuminate\Database\Eloquent\Model;
+
+class Comment extends Model
+{
+    //
+    public function post(){
+        return $this->belongsTo(Post::Class);
+    }
+    
+}
--- app/Post.php
+++ app/Post.php
@@ -2,10 +2,18 @@
 
 namespace App;
 use App\User;
+use App\Comment;
+use App\Tag;
 use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
     //
+    public function comments(){
+        return $this->hasMany(Comment::class);
+    }
+    public function tags(){
+        return $this->belongsToMany(Tag::class);
+    }
 
 }
